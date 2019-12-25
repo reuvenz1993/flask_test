62b41fe76df7b232d39975541982789c29a1bf1f
reuvenz1993 <rovenroven1@gmail.com>
2019-12-25 13:00:17 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
 
 app=Flask(__name__)
-
+app.config['SECRET_KEY'] = 'mysecret'
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
